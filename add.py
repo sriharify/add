6109974d3ed28ba4ef5d734aff5d8fe5dce690e4
sriharify <srihari.frontyard@gmail.com>
2020-11-23 18:40:17 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template,request,make_response,jsonify
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def add():
+    a=request.form['a']
+    b=request.form['b']
+    c=a+b
+    return{'result':c}
